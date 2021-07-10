f0212f4760c9313d2fd75aa87dac2a782e8bc1ec
Kelvin O. Lim <kolim@umn.edu>
2021-07-10 12:38:40 -0500

    add code

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 test app to upload a file to a rest api server written in flask.
 
+App is based on https://pub.dev/packages/multipart_request/example
+
+
+### code for the rest api server
 https://roytuts.com/python-flask-rest-api-file-upload/
 
 
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:multipart_request/multipart_request.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String imagePath = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Pick an image"),
+                onPressed: () async {
+                  File image =
+                  await ImagePicker.pickImage(source: ImageSource.gallery);
+                  imagePath = image.path;
+                },
+              ),
+              FlatButton(
+                child: Text("Call multipart request"),
+                onPressed: () {
+                  sendRequest();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void sendRequest() {
+    var request = MultipartRequest();
+
+    request.setUrl("http://192.168.1.33:5000/file-upload");
+    request.addFile("file", imagePath);
+
+    Response response = request.send();
+
+    response.onError = () {
+      print("Error");
+    };
+
+    response.onComplete = (response) {
+      print(response);
+    };
+
+    response.progress.listen((int progress) {
+      print("progress from response object " + progress.toString());
+    });
+  }
+}
